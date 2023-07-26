e733afc266208f87973af1b1528bfe65354629d6
Kevin Chen <kevin@chenbc.me>
2023-07-26 20:58:30 +0800

    fix: relocation target not defined error (#1)

--- rt/linkname.go
+++ rt/linkname.go
@@ -4,22 +4,25 @@ import (
 	"unsafe"
 )
 
-type mutex struct {
-	_ struct{} //lockRankStruct
-	_ uintptr  // key
-}
-
 //go:linkname allpptr runtime.allp
 var allpptr uintptr
 
 //go:linkname allpLock runtime.allpLock
-var allpLock mutex
+var allpLock uintptr
 
 //go:linkname lock2 runtime.lock2
-func lock2(*mutex)
+func lock2(*uintptr)
 
 //go:linkname unlock2 runtime.unlock2
-func unlock2(*mutex)
+func unlock2(*uintptr)
+
+func lockallp() {
+	lock2(&allpLock)
+}
+
+func unlockallp() {
+	unlock2(&allpLock)
+}
 
 func getgptr() unsafe.Pointer
 
